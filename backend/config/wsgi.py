68277d3a6e2642173d2f9ff5074d652a405a3828
felipeddiaz <felipeddbeca@gmail.com>
2026-04-20 20:33:33 -0600

    fix procfile to migrations db

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -14,3 +14,14 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
+
+
+#Alternativa más “pro” (para después)
+
+#Cuando escales:
+
+#usar:
+
+#[deploy]
+#startCommand = "gunicorn config.wsgi:application --bind 0.0.0.0:$PORT"
+#releaseCommand = "python manage.py migrate --no-input"
